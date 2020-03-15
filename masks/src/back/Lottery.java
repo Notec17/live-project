2bd28768f8c7f5f2929e6475618ef6d06a11636b
XTG-L <xtg-l@foxmail.com>
2020-03-15 10:28:34 +0800

    Lottery.java Done？

--- masks/src/back/Lottery.java
+++ masks/src/back/Lottery.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lottery {
+
+    /**
+     * 获取中号的List 并且传进来的参数会处理成没有摇中的List
+     * @param list 参与摇号的列表
+     * @param num 摇号的数量
+     * @return
+     */
     static List<String> Runlottery(List<String> list, int num) {
         List<String> result = new ArrayList<>();
         while (num-- > 0 && list.size() > 0) {
