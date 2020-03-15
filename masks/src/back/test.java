e161bc23cbe2e7805ccc44eec0d060f98c271e22
mowangaa <61011863+mowangaa@users.noreply.github.com>
2020-03-15 13:22:19 +0800

    手机号和身份证号重复问题验证

--- masks/src/back/test.java
+++ masks/src/back/test.java
@@ -9,16 +9,28 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class test {
-	private String username ="root";
-	private String password ="123456";
-	private String url ="jdbc:mysql://localhost:3306/D6plus";
-	public Connection Connecteddatabase() throws SQLException{
+	private static String username ="root";
+	private static String password ="123456";
+	private static String url ="jdbc:mysql://localhost:3306/D6plus";
+	public static Connection Connecteddatabase() throws SQLException{
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		}catch(ClassNotFoundException e){
 			System.out.println();
 		}
 		return DriverManager.getConnection(url,username,password);
-		
+	}
+	static boolean id_phnum_test(int n,String id,String phnum) throws SQLException {
+		Connection connection =Connecteddatabase();
+		Statement statement =connection.createStatement();
+		String sql1 = "SELECT * FROM nlist WHERE n='"+n+"' and id ='"+id+"' ";
+		String sql2 = "SELECT * FROM nlist WHERE n='"+n+"' and id ='"+phnum+"' ";
+		ResultSet rs1=statement.executeQuery(sql1);
+		ResultSet rs2=statement.executeQuery(sql2);
+		if(rs1==null&&rs2==null) {
+			return true;
+		}
+		else
+		  return false;
 	}
 }
