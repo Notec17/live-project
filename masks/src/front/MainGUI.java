fcf30a50760ecae3a6251fdd5603233ca689a840
huangmaoxian <847601978@qq.com>
2020-03-15 10:54:02 +0800

    Create MainGUI.java

---
+++ masks/src/front/MainGUI.java
@@ -0,0 +1,25 @@
+package front;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainGUI {
+    private JButton order;
+    private JButton query;
+
+    public MainGUI() {
+        order.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        query.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
